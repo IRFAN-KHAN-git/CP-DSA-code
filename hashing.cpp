ac5039619748e9edee2f4b9b9e2d780dcf5071de
IRFAN-KHAN-git <irfanprof9@gmail.com>
2024-11-10 19:38:42 +0530

    Update hashing.cpp

--- hashing.cpp
+++ hashing.cpp
@@ -4,3 +4,33 @@ using namespace std;
 
 int main()
 {
+	int t;
+	cin>>t;
+	while(t--){
+		int n,m;
+		int hash[26]={0};
+		cin>>n>>m;
+		string s1,s2;
+		cin>>s1>>s2;
+		for (int i = 0; i < n; ++i)
+		{
+			hash[s1[i]-'a']++;
+		}
+		
+		for (int j = 0; j < m; ++j)
+		{
+			hash[s2[j]-'a']++;
+		}
+		sort(hash,hash+26);
+		if(hash[0]==0){
+			cout<<"YES"<<"\n";
+		}
+		else{
+			cout<<"NO"<<"\n";
+		}
+
+	}			
+	
+
+	
+}
